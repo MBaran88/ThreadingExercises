357972dc65bcb4d6ad6a1618adbf90edbaedb88f
MBaran88 <zdzibq@gmail.com>
2017-06-21 19:30:11 +0200

    Inital commit

---
+++ src/main/java/Threading/App.java
@@ -0,0 +1,14 @@
+package Threading;
+
+/**
+ * Trzeba zrobić taki sam program jak na stronie http://www.cs.carleton.edu/faculty/dmusican/cs348/java_multi/SumThread.java
+ * tyle że w tablicy powinien znajdować największą i najmniejszą wartość tablicy
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){
+
+}
+}
+
---
+++ src/main/java/Threading/HighestLowestValueFinder.java
@@ -0,0 +1,11 @@
+package Threading;
+
+/**
+ * Created by RENT on 2017-06-21.
+ */
+public class HighestLowestValueFinder extends Thread {
+private int lo, hi;
+private int arr;
+private int lowestValue;
+private int highestValue;
+}
